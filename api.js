bf7be089d907cdef84656c5901a872b14b6bf566
AashikNR <ashiknizam77@gmail.com>
2020-06-12 15:25:45 +0530

    4  api methods

--- api.js
+++ api.js
@@ -10,8 +10,52 @@ function insertdataAsyn(data) {
     document.getElementById("api").appendChild(text)
     for (var i = 0; i < data.table.length; i++) {
       var div = document.createElement("div");
-      div.innerHTML = 'UserId: ' + data.table[i].id +' Title: ' + data.table[i].value;
+      var btn = document.createElement("BUTTON");
+      btn.setAttribute("id" , data.table[i].id);
+      btn.setAttribute("onClick" ,"deleteValue();");
+      btn.innerHTML = "DELETE";
+      var btnedit = document.createElement("BUTTON");
+      btnedit.setAttribute("id" , data.table[i].id);
+      btnedit.setAttribute("value" , data.table[i].value);
+      btnedit.setAttribute("onClick" ,"editValue();");
+      btnedit.innerHTML = "EDIT";
+      div.innerHTML = 'UserId: ' + data.table[i].id +' Title: ' + data.table[i].value + '<hr>';
+      document.getElementById("api").appendChild(btn);
+      document.getElementById("api").appendChild(btnedit);
       document.getElementById("api").appendChild(div);
     }
 }
 dataAsync();
+
+function getInputValue(){
+    var inputVal = document.getElementById("myInput").value;
+    fetch('http://127.0.0.1:3000/todo/add/'+inputVal,{
+        method: 'POST',
+    });
+}
+
+function deleteValue(){
+    fetch('http://127.0.0.1:3000/todo/delete/'+event.srcElement.id,{
+        method: 'DELETE'
+    });
+}
+
+
+function editValue(){
+
+    document.getElementById("myBtn_edit").style.display = "block";
+    document.getElementById("myBtn").style.display = "none";
+    let old_value = document.getElementById("myInput").value;
+    document.getElementById("myInput").value = event.srcElement.value;
+}
+
+function geteditedValue(){
+    // let old_value = "aa"
+    let new_value = document.getElementById("myInput").value
+    fetch('http://127.0.0.1:3000/todo/edit/'+old_value + '/' + new_value,{
+        method: 'PUT'
+    });
+    document.getElementById("myBtn").style.display = "block";
+    document.getElementById("myBtn_edit").style.display = "none";
+    document.getElementById("myInput").value = ""
+}
--- todoRoute.js
+++ todoRoute.js
@@ -2,57 +2,47 @@ let express = require('express');
 let router = express.Router();
 let fs = require('fs');
 
-let add = async (val)=>{
-   let obj = {
-       table: []
-    };
-    let json = JSON.stringify(obj);
-    fs.readFile('myjsonfile.json', 'utf8', (err, data)=>{
-       if (err){
-           console.log(err);
-       } else {
-       obj = JSON.parse(data);
-       obj.table.push({id:obj.table[obj.table.length-1].id+1, value:val});
-       json = JSON.stringify(obj);
-       fs.writeFile('myjsonfile.json', json, 'utf8', (err) => {
-           if (err) throw err;
-           console.log('Data added to file');
-       });
-   }});
-}
-
-let remove = async (val)=>{
-   let data =  await fs.readFileSync('myjsonfile.json');
-   let remove = val;
-   let json = JSON.parse(data);
-   let table = json.table;
-   json.table = table.filter((table) => { return table.id != remove });
-   fs.writeFileSync('myjsonfile.json', JSON.stringify(json, null, 2));
-   console.log("Deleted Successfully")
-}
-
-let list = async ()=>{
+router.get('/list', function(req, res){                     //list
    fs.readFile('myjsonfile.json', (err, data) => {
-       if (err) throw err;
-       let jsonData = JSON.parse(data);
-   });
+      if (err) throw err;
+      let jsonData = JSON.parse(data);
+      res.send(jsonData);
+  });
+});
+
+router.post('/add/:value', function(req, res){                  //add  //post
+   let obj = {
+      table: []
+   };
+   let json = JSON.stringify(obj);
+   fs.readFile('myjsonfile.json', 'utf8', (err, data)=>{
+      if (err){
+          console.log(err);
+      } else {
+      obj = JSON.parse(data);
+      obj.table.push({id:obj.table[obj.table.length-1].id+1, value:req.params.value});
+      json = JSON.stringify(obj);
+      fs.writeFile('myjsonfile.json', json, 'utf8', (err) => {
+          if (err) throw err;
+      });
+  }});
+  let json_data_ = JSON.parse(fs.readFileSync('myjsonfile.json'));
+  res.send(json_data_);
 }
+);
 
-const edit = async (val,new_value)=>{
-   const data = await fs.readFileSync('myjsonfile.json');
-   const remove = val;
+router.put('/edit/:old/:new', function(req, res){                     //edit  //put
+
+   const data = fs.readFileSync('myjsonfile.json');
+   const remove = req.params.old;
    var json = JSON.parse(data);
-
    for (let i = 0; i < json.table.length ; i++) {
-       if (json.table[i].value == val)
+       if (json.table[i].value == req.params.old)
            var id_var = json.table[i].id
        }
-
    const table = json.table;
    json.table = table.filter((table) => { return table.value !== remove });
    fs.writeFileSync('myjsonfile.json', JSON.stringify(json, null, 2));
-   console.log("Edited Successfully")
-
    let obj = {
        table: []
     };
@@ -62,33 +52,24 @@ const edit = async (val,new_value)=>{
            console.log(err);
        } else {
        obj = JSON.parse(data);
-       obj.table.push({id:id_var, value:new_value});
+       obj.table.push({id:id_var, value:req.params.new});
        json = JSON.stringify(obj);
        fs.writeFile('myjsonfile.json', json, 'utf8', (err) => {
            if (err) throw err;
        });
    }});
-
-}
-
-router.get('/list', function(req, res){ //list
-   fs.readFile('myjsonfile.json', (err, data) => {
-      if (err) throw err;
-      let jsonData = JSON.parse(data);
-      res.send(jsonData);
-  });
+   let json_data = JSON.parse(fs.readFileSync('myjsonfile.json'));
+   res.send(json_data);
 });
 
-router.post('/add/:value', function(req, res){ //add
-   res.send(add(req.params.value));
-});
-
-router.put('/edit/:old/:new', function(req, res){ //edit
-   res.send(edit(req.params.old,req.params.new));
-});
-
-router.delete('/delete/:id', function(req, res){ //delete
-   res.send(remove(req.params.id));
+router.delete('/delete/:id', function(req, res){                     //delete //delete
+   let data =  fs.readFileSync('myjsonfile.json');
+   let remove = req.params.id;
+   let json = JSON.parse(data);
+   let table = json.table;
+   json.table = table.filter((table) => { return table.id != remove });
+   fs.writeFileSync('myjsonfile.json', JSON.stringify(json, null, 2));
+   res.send(json);
 });
 
 module.exports = router;
